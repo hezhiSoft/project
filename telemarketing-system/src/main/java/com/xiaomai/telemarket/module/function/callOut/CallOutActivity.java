67fed138252c1e6bb2f350de6aa5aba76ff38da2
hezhiWu <wuhezhi007@gmail.com>
2017-06-04 13:59:33 +0800

    添加群呼状态监听，处理用户编辑信息逻辑

--- telemarketing-system/src/main/java/com/xiaomai/telemarket/module/function/callOut/CallOutActivity.java
+++ telemarketing-system/src/main/java/com/xiaomai/telemarket/module/function/callOut/CallOutActivity.java
@@ -2,11 +2,16 @@ package com.xiaomai.telemarket.module.function.callOut;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
 
 import com.jinggan.library.base.BaseActivity;
 import com.xiaomai.telemarket.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
+ * 员工外呼
  * author: hezhiWu <wuhezhi007@gmail.com>
  * version: V1.0
  * created at 2017/5/28$ 下午12:23$
@@ -16,10 +21,20 @@ import com.xiaomai.telemarket.R;
 
 public class CallOutActivity extends BaseActivity {
 
+    @BindView(R.id.dl_left)
+    DrawerLayout drawerLayout;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_out);
-        setToolbarTitle("员工外呼");
+        setToolbarRightImage(R.mipmap.icon_screen);
+        setToolbarTitle("外呼统计");
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClickToolbarRightLayout() {
+        super.onClickToolbarRightLayout();
     }
 }
